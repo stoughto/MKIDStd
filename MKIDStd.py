6681486589b7ca4e7a7865767bb362b40592404d
Chris Stoughton <chris.stoughton@gmail.com>
2013-01-18 08:27:50 -0600

    getVegaMag and change units on hd93521

--- MKIDStd.py
+++ MKIDStd.py
@@ -8,7 +8,7 @@ import pyfits
 import smooth
 import sys
 from scipy.constants import *
-
+import math
 class MKIDStd:
     """
     This class contains the spectra of several standard stars. These
@@ -39,11 +39,12 @@ class MKIDStd:
         self.lymanwavelengths = [1216,1026,973,950,938,931,926,923,921,919]
 
         self._loadFilterFile()
-        self.k = (1*10**-10/1*10**7)/h/c
-        """
-        h is in Joules/sec and c is in meters/sec. This k value is used in all unit conversions
-        """
 
+        
+        # h is in Joules/sec and c is in meters/sec. 
+        # This k value is used in conversions between counts and ergs
+        self.k = (1*10**-10/1*10**7)/h/c
+        self.vegaInCounts = "not loaded yet"
 
     def _loadFilterFile(self):
         filterFileName = os.path.join(self.this_dir,"data","ph08_UBVRI.mht")
@@ -114,15 +115,30 @@ class MKIDStd:
     def measureBandPassFlux(self,aFlux,aFilter):
         sum = 0
         sumd = 0
-		filter = numpy.interp(aFlux[:,0], aFilter[0,:], aFilter[1,:], 0, 0)
+        filter = numpy.interp(aFlux[:,0], aFilter[0,:], aFilter[1,:], 0, 0)
         for i in range(aFlux[:,0].size-1):
             dw = aFlux[i+1,0] - aFlux[i,0]
             flux = aFlux[i,1]*filter[i]/aFlux[i,0]
             sum += flux*dw
             sumd += filter[i]*dw
-            sum /= self.k
-            sum /= sumd
+        sum /= self.k
+        sum /= sumd
         return sum
+
+    def getVegaMag(self, aFlux, aFilter):
+        if self.vegaInCounts == "not loaded yet":
+            self.vegaInCounts = self.load("vega")
+        sumNumerator = 0.0
+        sumDenominator = 0.0
+        filter = numpy.interp(aFlux[:,0], aFilter[0,:], aFilter[1,:], 0, 0)
+        vFlux = numpy.interp(
+            aFlux[:,0], self.vegaInCounts[:,0], self.vegaInCounts[:,1], 0, 0)
+        for i in range(aFlux[:,0].size-1):
+            dw = aFlux[i+1,0] - aFlux[i,0]
+            sumNumerator += aFlux[i,1]*filter[i]*dw
+            sumDenominator += vFlux[i]*filter[i]*dw
+        mag = -2.5*math.log10(sumNumerator/sumDenominator) + 0.03
+        return mag
 
     def plot(self,name="all",xlog=False,ylog=True,xlim=[3000,10000],normalizeFlux=True,countsToErgs=False):
         """
@@ -258,7 +274,7 @@ class MKIDStd:
             retval[i][1] = f[1].data[i][0]
         return retval
 
-    def report(self, xlim=[500,10000000]):
+    def report(self):
         """
         Creates a text document that reports the units, citation, and
         description of each object
@@ -276,19 +292,17 @@ class MKIDStd:
 	    points = a[:,1].size
             x = a[:,0]
             y = a[:,1]
-	    imin = numpy.searchsorted(x,xlim[0])
-            imax = numpy.searchsorted(x,xlim[1])
-            xtemp = x[imin:imax]
-            xmin = abs(xtemp).min()
-            xmax = xtemp.max()
-	    WavelengthMin = xmin
-	    WavelengthMax = xmax
-	    print "---------------------------------------------------------------------------------------"
+            xmin = x.min()
+            xmax = x.max()
+            bMag = self.getVegaMag(a,self.filters['B'])
+            vMag = self.getVegaMag(a,self.filters['V'])
+            bmv = bMag - vMag
+	    print "------------------------------------------------------------"
 	    print "Name: %s" %name
 	    print "Units: Flux: %s Wavelength: %s " %(fluxUnit, wavelengthUnit) 
 	    print "Citation: %s" %citation
 	    print "Description: %s." %description
-	    print "Number of Points: %d Wavelength: Max =%9.3f Min = %10.3f" %(points, WavelengthMin, WavelengthMax)
-	    
+            print "V=%f  B-V=%f" % (vMag, bmv)
+	    print "Number of Points: %d Wavelength: Max =%9.3f Min = %10.3f" %(points, xmin, xmax)
 	sys.stdout = old_stdout
 	log_file.close()
--- test/TestABMag.py
+++ test/TestABMag.py
@@ -5,12 +5,47 @@ import MKIDStd
 class TestAB(unittest.TestCase):
 
     def testMeasureBandPassFlux(self):
+        # taken from "Spectrophotometry and Magnitudes by Chris Churchill
+        # table 5.1
+        # http://ganymede.nmsu.edu/cwc/Teaching/ASTR605/Lectures/mags-extinct.pdf
+
+        answers = { 'U':3.18e-9, 'B':6.60e-9, 'V':3.64e-9, 'R':3.08e-9, 'I':2.55e-9}
         std = MKIDStd.MKIDStd()
         aFlux = std.load("vega")
-        aFilter = std.filters['U']
-        bpf = std.measureBandPassFlux(aFlux,aFilter)
-        print "bpf=",bpf
+        for filter in ['U','B','V','R','I']:
+            aFilter = std.filters[filter]
+            bpf = std.measureBandPassFlux(aFlux,aFilter)
+            print "filter=",filter,"  answer=",answers[filter], "  bpf=",bpf
 
+    def testGetVegaMag(self):
+        """
+        confirm that the magnitude of Vega in all bands is 0.03
+        """
+        std = MKIDStd.MKIDStd()
+        vegaFlux = std.load("vega")
+        bd17Flux = std.load("bd17")
+        for filter in ['U','B','V','R','I']:
+            aFilter = std.filters[filter]            
+            mag = std.getVegaMag(vegaFlux, aFilter)
+            self.assertAlmostEqual(0.03, mag, msg="filter=%s mag=%f"%(filter,mag))
+
+    def testCalspecMags(self):
+        """
+        compare values from getVegaMag to values in
+        http://www.stsci.edu/hst/observatory/cdbs/calspec.html
+        """
+        std = MKIDStd.MKIDStd()
+        bFilter = std.filters['B']
+        vFilter = std.filters['V']
+
+        # BD17
+        bd17Flux = std.load("bd17")
+        B = std.getVegaMag(bd17Flux, bFilter)
+        V = std.getVegaMag(bd17Flux, vFilter)
+        self.assertAlmostEqual(B-V, 0.44, places=1, msg="value=%f"%B)
+        self.assertAlmostEqual(B, 9.47, places=0, msg="value=%f"%B)
+
+        
     def testConvert(self):
         """
         Test the equation for converting AB magnitude to flux and back again, 
